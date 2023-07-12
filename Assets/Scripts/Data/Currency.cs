67077b593ed7637edd67ac2a8f753d0a14bb1e20
Viktor Rodrigo <rodrigo.victor@yandex.ru>
2023-07-12 16:10:14 +0300

    added Currency to save data

---
+++ Assets/Scripts/Data/Currency.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Data
+{
+    [Serializable]
+    public class Currency
+    {
+        public int Money;
+    }
+}
--- Assets/Scripts/Data/PlayerProgress.cs
+++ Assets/Scripts/Data/PlayerProgress.cs
@@ -6,10 +6,12 @@ namespace Data
     public class PlayerProgress
     {
         public WorldData WorldData;
+        public Currency Currency;
 
         public PlayerProgress(string initialLevel)
         {
             WorldData = new WorldData(initialLevel);
+            Currency = new Currency();
         }
     }
 }
