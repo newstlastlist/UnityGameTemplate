b1453e25ac44156aa3ec3a44e55dbc35ab44aad7
Viktor Rodrigo <rodrigo.victor@yandex.ru>
2023-07-14 16:07:46 +0300

    dont destroy on loaded loading screen ui

--- Assets/Scripts/Infrastructure/UI/LoadingScreen.cs
+++ Assets/Scripts/Infrastructure/UI/LoadingScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,5 +7,10 @@ namespace Infrastructure.UI
     public class LoadingScreen : MonoBehaviour
     {
         [SerializeField] private Slider _loadingProgressBar;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
     }
 }
