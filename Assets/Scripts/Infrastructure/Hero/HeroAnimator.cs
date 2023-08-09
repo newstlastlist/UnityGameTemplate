296c8fbc87e56840c27d2daa3a8181ecb253afe4
Viktor Rodrigo <rodrigo.victor@yandex.ru>
2023-08-09 21:35:00 +0300

    character animator

---
+++ Assets/Scripts/Infrastructure/Hero/HeroAnimator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Logic;
+using UnityEngine;
+
+namespace Infrastructure.Hero
+{
+    public class HeroAnimator : MonoBehaviour, IAnimationStateReader
+    {
+        //example
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] public Animator _animator;
+
+        private readonly int _idleStateHash = Animator.StringToHash("Idle");
+        private readonly int _runningStateHash = Animator.StringToHash("Run");
+
+        public event Action<AnimatorState> StateEntered;
+        public event Action<AnimatorState> StateExited;
+
+        public AnimatorState State { get; private set; }
+
+        private void Update()
+        {
+            _animator.SetFloat(_runningStateHash, _characterController.velocity.magnitude, 0.1f, Time.deltaTime);
+        }
+
+        public void ResetToIdle()
+        {
+            _animator.Play(_idleStateHash, -1);
+        }
+
+        public void EnteredState(int stateHash)
+        {
+            State = StateFor(stateHash);
+            StateEntered?.Invoke(State);
+        }
+
+        public void ExitedState(int stateHash)
+        {
+            StateExited?.Invoke(StateFor(stateHash));
+        }
+
+        private AnimatorState StateFor(int stateHash)
+        {
+            AnimatorState state;
+            if (stateHash == _idleStateHash)
+            {
+                state = AnimatorState.Idle;
+            }
+            else if (stateHash == _runningStateHash)
+            {
+                state = AnimatorState.Run;
+            }
+            else
+            {
+                state = AnimatorState.Unknown;
+            }
+
+            return state;
+        }
+    }
+}
---
+++ Assets/Scripts/Logic/AnimatorState.cs
@@ -0,0 +1,9 @@
+﻿namespace Logic
+{
+    public enum AnimatorState
+    {
+        Idle,
+        Run,
+        Unknown
+    }
+}
